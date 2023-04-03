bf621eae16c8c1cb1881112d636abd76df8a9c1f
Adaminou <200537@site.asty-moulin.com>
2023-04-04 01:20:06 +0200

    Essai

---
+++ AppliMatrice/MethodeTraitementMat.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace AppliMatrice
+{
+    public struct MethodeTraitementMat
+    {
+        public void RempliTabMat(out int[,] tableau, int ligne, int colonne)
+        {
+            Random alea = new Random();
+            tableau = new int[ligne, colonne];
+            for (int i = 0; i < ligne; i++)
+            {
+                for (int j = 0; j < colonne; j++)
+                {
+                    tableau[i, j] = alea.Next(ligne, colonne);
+                }
+            }
+        }
+
+        public void ConcatTabMat(int[,] tableau, out string chaine, int colonne, int ligne)
+        {
+            chaine = "";
+            for (int i = 0; i < ligne; i++)
+            {
+                for (int j = 0; j < colonne; j++)
+                {
+                    chaine = chaine + tableau[i, j] + ", ";
+                }
+            }
+        }
+
+        public void AddTabMat(int[,] tableau1, int[,] tableau2,int [,]tableau3)
+        {
+            if (tableau1.GetLength(0) == tableau2.GetLength(0) && tableau1.GetLength(1) == tableau2.GetLength(1))
+            {
+                for (int i = 0; i < tableau1.GetLength(0) - 1; i++)
+                {
+                    for (int j = 0; j < tableau2.GetLength(0) - 1; j++)
+                    {
+                        tableau3[i, j] = tableau1[i, j] + tableau2[i, j];
+                    }
+                }
+                bool resultat = true;
+            }
+            else
+            {
+                bool resultat = false;
+            }
+        }
+
+
+
+    }
+
+}
---
+++ AppliMatrice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AppliMatrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tableau;
+            int[,] tableau1;
+            int[,] tableau2;
+            int[,] tableau3;
+            int colonne;
+            int ligne;
+            string chaine;
+            string choix;
+            
+        }
+        MethodeTraitementMat MesOutils = new MethodeTraitementMat();
+        do
+	    {
+            /*Console.WriteLine("Quelles sont vos dimensions minimale ?");
+            ligne,colonne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quelles sont vos dimensions maximale ?");
+            ligne,colonne = int.Parse(Console.ReadLine());
+            MesOutils.RempliTabMat(out tableau,ligne, colonne);
+            MesOutils.ConcatTabMat(int tableau,string chaine, int colonne, int ligne);
+            Console.WriteLine(chaine);
+
+            Console.WriteLine("Les matrices);
+            Console.WriteLine("=========================");
+
+            Console.WriteLine("Notre menu :" +
+
+            "\n    1. Addition de matrice" +
+            "\n    2. Multiplication de matrice");
+            choix = Console.ReadLine();*/
+
+	    } 
+        while (true);
+    }
+}
